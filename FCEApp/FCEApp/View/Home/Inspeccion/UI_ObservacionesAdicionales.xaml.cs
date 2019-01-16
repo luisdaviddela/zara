36fc28ba75ad563490ed4a1329b8264851666b3c
Luis David De La Cruz Bautista <delacruz_luis@outlook.es>
2019-01-16 13:41:12 -0600

    Observaciones adicionales

---
+++ FCEApp/FCEApp/View/Home/Inspeccion/UI_ObservacionesAdicionales.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FCEApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class UI_ObservacionesAdicionales : ContentPage
+	{
+		public UI_ObservacionesAdicionales ()
+		{
+			InitializeComponent ();
+            FinalizarBtn.IsEnabled = false;
+        }
+
+        private void Editor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var KeyWord = ObsAdicion.Text;
+            if (KeyWord.Length >3)
+            {
+                FinalizarBtn.IsEnabled = true;
+            }
+            else
+            {
+                FinalizarBtn.IsEnabled = false;
+            }
+        }
+    }
+}
